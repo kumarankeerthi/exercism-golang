f05ca40e1872ee8f64055de79723cbe8431491d5
Keerthi Kumaran <keerthi.kumaran@metlife.com>
2019-04-02 16:32:28 -0400

    refactoring raindrops excercise

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -8,19 +8,15 @@ import (
 // Convert returns a string that is ia combination of "Pling" , "Plang" , "Plong" based on the factors of a given number
 func Convert(n int) string {
 	var result string
-	factors := []int{}
 	for i := 1; i <= n; i++ {
 		if n%i == 0 {
-			factors = append(factors, i)
-		}
-	}
-	for _, v := range factors {
-		if v == 3 {
-			result += "Pling"
-		} else if v == 5 {
-			result += "Plang"
-		} else if v == 7 {
-			result += "Plong"
+			if i == 3 {
+				result += "Pling"
+			} else if i == 5 {
+				result += "Plang"
+			} else if i == 7 {
+				result += "Plong"
+			}
 		}
 	}
 	if result == "" {
